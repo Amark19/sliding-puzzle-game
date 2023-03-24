cc1bcbdcc28a60e9ae592390630fa8bb7f744c95
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2023-03-24 23:57:59 +0530

    Update ARPlacement.cs

--- Assets/Scripts/ARPlacement.cs
+++ Assets/Scripts/ARPlacement.cs
@@ -28,13 +28,10 @@ public class ARPlacement : MonoBehaviour
         {
             ARPlaceObject();
         }
-
-
         UpdatePlacementPose();
         UpdatePlacementIndicator();
-
-
     }
+    
     void UpdatePlacementIndicator()
     {
         if(spawnedObject == null && placementPoseIsValid)
@@ -67,11 +64,10 @@ public class ARPlacement : MonoBehaviour
         arObjectToSpawn.transform.SetPositionAndRotation(new Vector3(PlacementPose.position.x,PlacementPose.position.y + 1f,PlacementPose.position.z), PlacementPose.rotation);
         placementIndicator.SetActive(false);
         var planeManager = this.GetComponent<ARPlaneManager>();
-foreach (var plane in planeManager.trackables)
-{
-    plane.gameObject.SetActive(false);
-}
-
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
         this.GetComponent<ARPlaneManager>().enabled=false;
         this.GetComponent<ARPlane>().enabled=false;
         HelperOverlay.SetActive(false);
